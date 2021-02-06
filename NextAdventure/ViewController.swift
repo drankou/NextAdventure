9e30c70ab03cfcc8852c41eab4adfda53d93935c
Aliaksandr Drankou <drankovalexander@gmail.com>
2021-02-06 15:04:36 +0100

    Initial Commit

---
+++ NextAdventure/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NextAdventure
+//
+//  Created by Aliaksandr Drankou on 06.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
